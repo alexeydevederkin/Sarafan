da2f2aab16f8545e76fff40ecabb49a64d08c8b3
alexeydevederkin <asd.correo@gmail.com>
2019-07-19 14:05:52 +0700

    added feature: subscriptions & subscribers

--- src/main/java/com/asd/sarafan/repo/UserDetailsRepo.java
+++ src/main/java/com/asd/sarafan/repo/UserDetailsRepo.java
@@ -1,10 +1,15 @@
 package com.asd.sarafan.repo;
 
 import com.asd.sarafan.domain.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface UserDetailsRepo extends JpaRepository<User, String> {
+    @EntityGraph(attributePaths = {"subscriptions", "subscribers"})
+    Optional<User> findById(String s);
 }
---
+++ src/main/java/com/asd/sarafan/service/ProfileService.java
@@ -0,0 +1,30 @@
+package com.asd.sarafan.service;
+
+import com.asd.sarafan.domain.User;
+import com.asd.sarafan.repo.UserDetailsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ProfileService {
+    private final UserDetailsRepo userDetailsRepo;
+
+    @Autowired
+    public ProfileService(UserDetailsRepo userDetailsRepo) {
+        this.userDetailsRepo = userDetailsRepo;
+    }
+
+    public User changeSubscription(User channel, User subscriber) {
+        Set<User> subscribers = channel.getSubscribers();
+
+        if (subscribers.contains(subscriber)) {
+            subscribers.remove(subscriber);
+        } else {
+            subscribers.add(subscriber);
+        }
+
+        return userDetailsRepo.save(channel);
+    }
+}
