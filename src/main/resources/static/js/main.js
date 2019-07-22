6a551f5f0e5382bf29c50664cf21b055bacc872f
alexeydevederkin <asd.correo@gmail.com>
2019-07-22 19:26:55 +0700

    added Sentry to project

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,10 +7,28 @@ import App from 'pages/App.vue'
 import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+
+
+Sentry.init({
+    dsn: 'https://251231fa90e94cdfb9be431d6af30017@sentry.io/1510391',
+    integrations: [new Integrations.Vue({Vue, attachProps: true})],
+})
+
+
+Sentry.configureScope(scope =>
+    scope.setUser({
+        id: profile && profile.id,
+        username: profile && profile.name
+    })
+)
+
 
 if (profile) {
     connect()
 }
+
 
 Vue.use(Vuetify)
 
